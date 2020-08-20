42400af36b5b9f11584cbc44030027b3f71ddf13
avinash2678 <avinash2678@gmail.com>
2020-08-20 17:47:21 +0530

    Updates

---
+++ precache-manifest.2157e9f2286851e13483466b59cc4ae5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad566b9e579d8aaefdf0467eb94ae95d",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "35d8acd8b17de6b0e183",
+    "url": "/netflix-clone/static/css/main.d5a4e537.chunk.css"
+  },
+  {
+    "revision": "9cb745c28369b626ee43",
+    "url": "/netflix-clone/static/js/2.396b7e84.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.396b7e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35d8acd8b17de6b0e183",
+    "url": "/netflix-clone/static/js/main.85790bbd.chunk.js"
+  },
+  {
+    "revision": "42ccba1c2e183f5632cb",
+    "url": "/netflix-clone/static/js/runtime-main.baddefd0.js"
+  }
+]);
